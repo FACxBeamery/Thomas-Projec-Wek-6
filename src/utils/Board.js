be868ef6b53c896fb9afa0230eae46f3eb117e67
TKostrzewski <thomas_kostrzewski@hotmail.com>
2019-10-17 13:54:19 +0100

    Initialise board for Tic Tac Toe

--- src/utils/Board.js
+++ src/utils/Board.js
@@ -1,12 +1,52 @@
 import React from "react";
 import styles from "../index.css";
 
+const Square = (props) => {
+	return (
+		<button
+			className={styles.square}
+			onClick={() => {
+				const nextSquares = [...squares];
+				squares[props.value] = "X";
+				setSquares(nextSquares);
+			}}
+		>
+			{props.value}
+		</button>
+	);
+};
+
 const Board = (props) => {
+	const status = "Next player: X";
 	return (
 		<div>
-			<div className={styles.grid}>board2</div>
+			<div className={styles.status}>{status}</div>
+			<div className={styles.boardRow}>
+				<Square value={0} />
+				<Square value={1} />
+				<Square value={2} />
+			</div>
+			<div className={styles.boardRow}>
+				<Square value={3} />
+				<Square value={4} />
+				<Square value={5} />
+			</div>
+			<div className={styles.boardRow}>
+				<Square value={6} />
+				<Square value={7} />
+				<Square value={8} />
+			</div>
 		</div>
 	);
 };
 
-export default Board;
+const Game = (props) => {
+	const [squares, setSquares] = React.useState(Array(9).fill(null));
+	return (
+		<div>
+			<Board />
+		</div>
+	);
+};
+
+export default Game;
--- src/utils/board.css.js
+++
@@ -1,9 +0,0 @@
-const board = {
-    display: "grid";
-	grid-template-columns: "200vw";
-	grid-template-rows: "400vw";
-	background-color: "blue";
-	min-width: "100vw";
-	min-height: "100vw";
-
-}
